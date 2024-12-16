e97ef0f6ec1a7d548a1455b85962eb3ab0c85460
bwondessen <bwondessen23@gmail.com>
2024-12-16 17:17:42 -0600

    Add event listener to page

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Retrieve todo from local storage or initialize an empty array
-
 let todo = JSON.parse(localStorage.getItem("todo")) || [];
 
 const todoInput = document.getElementById("todo-input");
@@ -7,3 +6,12 @@ const todoList = document.getElementById("todo-list");
 const todoCount = document.getElementById("todo-count");
 const deleteButton = document.getElementById("delete-btn");
 const addButton = document.querySelector(".btn");
+
+// Initialize
+document.addEventListener("DOMContentLoaded", () => {
+    addButton.addEventListener("click", addTask);
+});
+
+function addTask() {
+
+};
