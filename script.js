dffeeab0f8251baf713ec479674888cf967120b9
bwondessen <bwondessen23@gmail.com>
2024-12-16 17:10:47 -0600

    Retrieve necessary elements via DOM

---
+++ script.js
@@ -0,0 +1,9 @@
+// Retrieve todo from local storage or initialize an empty array
+
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
+
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
+const todoCount = document.getElementById("todo-count");
+const deleteButton = document.getElementById("delete-btn");
+const addButton = document.querySelector(".btn");
